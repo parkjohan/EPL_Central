7d66a104d37b5fdfad29eb9e65a2d5c11d5c6ebf
Johan Park <parkjohan@gmail.com>
2021-05-04 11:53:45 -1000

    Added a bunch of handlebar template code
    We can switch it out when we feel ready

---
+++ app.js
@@ -0,0 +1,16 @@
+/*var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.require('.hbs', exphbs({
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res){
+    res.render('index')
+});*/
+
+// This is using handlebars and express for templating.
+// We can switch it out when we want making sure it works on flip server too.
