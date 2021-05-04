cdc9a2b972ec37234261863050ffe4134e4742eb
Johan Park <parkjohan@gmail.com>
2021-05-04 08:00:41 -1000

    added database connector and added mysql to package.json

--- handler.js
+++ handler.js
@@ -4,8 +4,13 @@ var app = express();
 
 app.use(express.static('project'));
 
+// http://flip3.engr.oregonstate.edu:6570/
+
 app.set('port', 6570);
 app.set('view engine', 'html');
+
+// Database
+var db = require('./database/db-connector')
 
 //app.get('/~shaibis/style.css',function(req,res){
 	//res.render('home-page');
@@ -18,8 +23,35 @@ app.set('view engine', 'html');
 //});
 
 app.get('/',function(req,res){
-	
-  res.sendFile('./index.html', {root : __dirname});
+
+  /******TESTING ******/
+	// Define queries
+  query1 = 'DROP TABLE IF EXISTS diagnostic;';
+  query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+  query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+  query4 = 'SELECT * FROM diagnostic;';
+
+  // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+  // DROP TABLE...
+  db.pool.query(query1, function (err, results, fields){
+
+    // CREATE TABLE...
+    db.pool.query(query2, function(err, results, fields){
+
+      // INSERT INTO...
+      db.pool.query(query3, function(err, results, fields){
+
+        // SELECT *...
+        db.pool.query(query4, function(err, results, fields){
+
+          // Send the results to the browser
+          res.send(JSON.stringify(results));
+          //res.sendFile('./index.html', {root : __dirname});
+        });
+      });
+    });
+  });
 });
 
 app.get('/~shaibis/teams',function(req,res){
