46389b5a8c2e30f539b224bef9193507145e1bb1
SaeedSS3 <71868627+SaeedSS3@users.noreply.github.com>
2021-06-06 01:23:10 -0400

    Create delete_team.js

---
+++ public/scripts/delete_team.js
@@ -0,0 +1,15 @@
+function deleteTeam() {
+    var req = new XMLHttpRequest();
+    var teamID = this.value;
+    var payload = {
+        'teamID': teamID
+    }
+    req.open('DELETE', '/delete-team/' + teamID, true);
+    req.addEventListener('load', function () {
+        if (req.status >= 200 && req.status < 400) {
+            console.log("Deleting...");
+            console.log(JSON.parse(req.responseText));
+        }
+    });
+    req.send(payload);
+}
